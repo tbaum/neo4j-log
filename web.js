9c3acf981d027679e2e4e926b075b17697fb9e99
Thomas Baum <thomas.baum@atns.de>
2011-12-23 11:21:11 +0100

    compiled

--- web.js
+++ web.js
@@ -1,12 +1,39 @@
-var express = require('express');
+(function() {
+  var connect;
 
-var app = express.createServer(express.logger());
+  connect = require('connect');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+  connect(connect.basicAuth(function(user, pass) {
+    return auth.user === user && auth.pass === pass;
+  }), connect.router(function(app) {
+    app.get('/', function(request, response) {
+      var instance, proxy, res;
+      res = {};
+      for (instance in proxies) {
+        proxy = proxies[instance];
+        res[instance] = proxy.running();
+      }
+      return response.end(JSON.stringify(res));
+    });
+    app.post('/:id', function(request, response) {
+      var id;
+      id = request.params.id;
+      if (proxies[id]) {
+        throw new Error("instance " + id + " is already registered");
+      }
+      bringUp(id);
+      storeConfig();
+      return response.end("add " + request.params.id);
+    });
+    return app["delete"]('/:id', function(request, response) {
+      var id;
+      id = request.params.id;
+      if (!proxies[id]) throw new Error("instance " + id + " is not registered");
+      proxies[id].stop();
+      delete proxies[id];
+      storeConfig();
+      return response.end("delete " + request.params.id);
+    });
+  })).listen(process.env.PORT || 3000);
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+}).call(this);
