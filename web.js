e083dd7d0567ef13ed30d06fa98911931975266b
Thomas Baum <thomas.baum@atns.de>
2011-12-23 11:23:19 +0100

    hooks

--- web.js
+++ web.js
@@ -3,36 +3,13 @@
 
   connect = require('connect');
 
-  connect(connect.basicAuth(function(user, pass) {
-    return auth.user === user && auth.pass === pass;
-  }), connect.router(function(app) {
-    app.get('/', function(request, response) {
-      var instance, proxy, res;
-      res = {};
-      for (instance in proxies) {
-        proxy = proxies[instance];
-        res[instance] = proxy.running();
-      }
+  connect(connect.router(function(app) {
+    return app.get('/', function(request, response) {
+      var res;
+      res = {
+        ok: 'for-me'
+      };
       return response.end(JSON.stringify(res));
-    });
-    app.post('/:id', function(request, response) {
-      var id;
-      id = request.params.id;
-      if (proxies[id]) {
-        throw new Error("instance " + id + " is already registered");
-      }
-      bringUp(id);
-      storeConfig();
-      return response.end("add " + request.params.id);
-    });
-    return app["delete"]('/:id', function(request, response) {
-      var id;
-      id = request.params.id;
-      if (!proxies[id]) throw new Error("instance " + id + " is not registered");
-      proxies[id].stop();
-      delete proxies[id];
-      storeConfig();
-      return response.end("delete " + request.params.id);
     });
   })).listen(process.env.PORT || 3000);
 
