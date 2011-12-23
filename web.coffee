8e3de21d538dafe127fd6f730270941449f3f301
Thomas Baum <thomas.baum@atns.de>
2011-12-23 13:00:15 +0100

    update

--- web.coffee
+++ web.coffee
@@ -1,27 +1,26 @@
 connect = require 'connect'
+http = require('http')
 
 class MyApp
 	constructor: ->
 		res = {}
-		@res = ()-> { res }
-		console.log "XXXXXXXXXXXXXXXXX"
-		console.log __dirname
-		console.log process.env
-		connect(
-#        connect.basicAuth (user, pass) -> auth.user == user && auth.pass == pass
+#		@res = ()-> { res }
+		app = connect(
+			connect.query()
 			connect.router (app) ->
 				app.get '/l', (request, response) ->
-					res['last'] = JSON.stringify(request.header)
+					res['query'] = request.query
+					res['last'] = request.header
 					res['url'] = request.url
 					res['originalUrl'] = request.originalUrl
-					response.end "OK"
-
-				app.get '/', (request, response) ->
-#                res = { ok : 'for-me' }
-#                res[instance] = proxy.running() for instance, proxy of proxies
+					response.setHeader('Content-Type', 'image/gif');
+					response.end "\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00"+
+								 "\x00\x00\x00\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00"+
+								 "\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b", 'binary'
+				app.get '/debug', (request, response) ->
 					response.end JSON.stringify(res)
-			connect.static(__dirname + '/public', { maxAge: 0 })
-		).listen(process.env.PORT || 3000)
+			connect.static(__dirname + '/public')
+		)
 
-
+		app.listen(process.env.PORT || 3000)
 s = new MyApp()
--- web.js
+++ web.js
@@ -1,34 +1,29 @@
 (function() {
-  var MyApp, connect, s;
+  var MyApp, connect, http, s;
 
   connect = require('connect');
+
+  http = require('http');
 
   MyApp = (function() {
 
     function MyApp() {
-      var res;
+      var app, res;
       res = {};
-      this.res = function() {
-        return {
-          res: res
-        };
-      };
-      console.log("XXXXXXXXXXXXXXXXX");
-      console.log(__dirname);
-      console.log(process.env);
-      connect(connect.router(function(app) {
+      app = connect(connect.query(), connect.router(function(app) {
         app.get('/l', function(request, response) {
-          res['last'] = JSON.stringify(request.header);
+          res['query'] = request.query;
+          res['last'] = request.header;
           res['url'] = request.url;
           res['originalUrl'] = request.originalUrl;
-          return response.end("OK");
+          response.setHeader('Content-Type', 'image/gif');
+          return response.end("\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00" + "\x00\x00\x00\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00" + "\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b", 'binary');
         });
-        return app.get('/', function(request, response) {
+        return app.get('/debug', function(request, response) {
           return response.end(JSON.stringify(res));
         });
-      }), connect.static(__dirname + '/public', {
-        maxAge: 0
-      })).listen(process.env.PORT || 3000);
+      }), connect.static(__dirname + '/public'));
+      app.listen(process.env.PORT || 3000);
     }
 
     return MyApp;
