8b1a99845dd00c2345d742c8b926b51b8a3a6f65
Thomas Baum <thomas.baum@atns.de>
2011-12-28 20:33:35 +0100

    x

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 require('coffee-script');
 
 var url = process.env.NEO4J_URL || 'http://81c130a01:4f382f810@856db9f68.hosted.neo4j.org:7006';
-var app = require("myapp.coffee");
+var app = require("./myapp");
 
 app.start(url);
